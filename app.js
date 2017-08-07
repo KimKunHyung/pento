dc5ea7815e4e52546841e57fa1dd35b19740717f
KimKunHyung <30647788+KimKunHyung@users.noreply.github.com>
2017-08-07 23:32:05 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const Bot = require('messenger-bot')
 const process = require('process')
 
 let bot = new Bot({
-  token: '<EAAPGp72nqMIBACDiNxILaaZBNsEP8Ot8TBq7Pg2n1bEvLbd45ZAke5khXeZBWAQe4PUCWoG43hWZCqGOR7LLdDZAANsMmfzNLZAzqOaH6QDFRNxVsHNmTN4zcFVsyK7m8zQSZCgfU4l9R4I4Epduif9dY0SaNgjfl8iDxH1l2JemgZDZD>' ,
+  token: 'EAAPGp72nqMIBACDiNxILaaZBNsEP8Ot8TBq7Pg2n1bEvLbd45ZAke5khXeZBWAQe4PUCWoG43hWZCqGOR7LLdDZAANsMmfzNLZAzqOaH6QDFRNxVsHNmTN4zcFVsyK7m8zQSZCgfU4l9R4I4Epduif9dY0SaNgjfl8iDxH1l2JemgZDZD',
   verify: 'helloworld',
-  app_secret: '<b3f439dd050d4ca00ab72e490b4021b4>'
+  app_secret: 'b3f439dd050d4ca00ab72e490b4021b4'
 })
 
 bot.on('error', (err) => {
